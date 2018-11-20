2a1b490a9dd7872082dc9daf4ca43e7dc4be95d1
Max-Wendel-UFC <maxwendelffaraujo@alu.ufc.br>
2018-11-20 11:37:28 -0300

    add some tests

--- src/test/java/br/com/emmanuelneri/monolitica/ItemPedido/ItemPedidoServiceUnitTest.java
+++ src/test/java/br/com/emmanuelneri/monolitica/ItemPedido/ItemPedidoServiceUnitTest.java
@@ -1,17 +1,25 @@
 package br.com.emmanuelneri.monolitica.ItemPedido;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import br.com.emmanuelneri.monolitica.model.ItemPedido;
+import br.com.emmanuelneri.monolitica.model.Veiculo;
 import br.com.emmanuelneri.monolitica.service.ItemPedidoService;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,11 +28,36 @@ public class ItemPedidoServiceUnitTest {
 	
 	@Mock ItemPedido itemPedido;
 	@Mock ItemPedidoService itemPedidoService;
+	@Mock Veiculo veiculo;
+	
+	ArrayList<ItemPedido> itens;
+	
+	@Before
+	public void setup() {
+		itens = new ArrayList<ItemPedido>();
+		itens.add(new ItemPedido(new BigDecimal("10"), 4, veiculo));
+		itens.add(new ItemPedido(new BigDecimal("11"), 5, veiculo));
+		itens.add(new ItemPedido(new BigDecimal("12"), 6, veiculo));
+		itens.add(new ItemPedido(new BigDecimal("13"), 7, veiculo));
+	}
+	
 	
 	@Test
 	public void TestarSalvarItemPedido() {
 		itemPedidoService.save(itemPedido);
 		when(itemPedidoService.findById(itemPedido.getId())).thenReturn(itemPedido);
 		verify(itemPedidoService, atMost(10)).save(itemPedido);
+	}
+	
+	@Test
+	public void TestarBuscarItemPedido() {
+		Mockito.when(itemPedidoService.findById((long) 1)).thenReturn(itens.get(0));
+		assertEquals(itemPedidoService.findById((long) 1), itens.get(0));
+	}
+	
+	@Test
+	public void TestarFindAllNaoNulo() {
+		Mockito.when(itemPedidoService.findAll()).thenReturn(itens);
+		assertNotNull(itemPedidoService.findAll());
 	}
 }
--- src/test/java/br/com/emmanuelneri/monolitica/Pedido/PedidoServiceUnitTest.java
+++ src/test/java/br/com/emmanuelneri/monolitica/Pedido/PedidoServiceUnitTest.java
@@ -1,9 +1,14 @@
 package br.com.emmanuelneri.monolitica.Pedido;
 
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
@@ -11,20 +16,61 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import br.com.emmanuelneri.monolitica.exception.ValidationException;
 import br.com.emmanuelneri.monolitica.model.Pedido;
+import br.com.emmanuelneri.monolitica.model.Usuario;
 import br.com.emmanuelneri.monolitica.service.PedidoService;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(MockitoJUnitRunner.class)
 public class PedidoServiceUnitTest {
-	
+
 	@Mock Pedido pedido;
 	@Mock PedidoService pedidoService;
-	
+	@Mock Usuario usuario;
+
+	ArrayList<Pedido> pedidos;
+
+	@Before
+	public void setup() {
+		pedidos = new ArrayList<Pedido>();
+		
+		usuario.setEmail("abc@abc.com");
+		usuario.setSenha("abc123");
+		pedidos.add(new Pedido(usuario));
+		
+		usuario.setEmail("email@email.com");
+		usuario.setSenha("email");
+		pedidos.add(new Pedido(usuario));
+		
+		usuario.setEmail("baba@baba.com");
+		usuario.setSenha("bababa");
+		pedidos.add(new Pedido(usuario));
+	}
+
 	@Test
-	public void TestaSalvarPedido() {
-		pedidoService.save(pedido);
-		when(pedidoService.findById(pedido.getId())).thenReturn(pedido);
+	public void SalvarTest() throws ValidationException {
+		pedidoService.salvar(pedido);
+		when(pedidoService.findById((long)1)).thenReturn(pedidos.get(0));
+		assertTrue(pedidoService.findById((long)1).equals(pedidos.get(0)));
 		verify(pedidoService, atMost(10)).save(pedido);
 	}
+
+	@Test
+	public void findPedidoCompletoByIdTest() throws ValidationException {
+		when(pedidoService.findById((long)2)).thenReturn(pedidos.get(1));		
+		verify(pedidoService, atMost(10)).update(pedido);
+	}
+
+	@Test
+	public void finalizarTest() {
+		verify(pedidoService, atMost(10)).finalizar(pedido);
+	}
+
+	@Test
+	public void FindPedidoCompletoByIdTest() {
+		when(pedidoService.findPedidoCompletoById(pedido.getId())).thenReturn(pedido);
+		verify(pedidoService, atMost(10)).findPedidoCompletoById(pedido.getId());
+	}
+
 }
