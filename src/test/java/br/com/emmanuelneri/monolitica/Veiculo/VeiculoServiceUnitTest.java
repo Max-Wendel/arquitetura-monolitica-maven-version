e5181ecef9fd60bb3567af39065a828e03fc786d
Max-Wendel-UFC <maxwendelffaraujo@alu.ufc.br>
2018-11-26 11:58:59 -0300

    Add unit tests

--- src/test/java/br/com/emmanuelneri/monolitica/Veiculo/VeiculoServiceUnitTest.java
+++ src/test/java/br/com/emmanuelneri/monolitica/Veiculo/VeiculoServiceUnitTest.java
@@ -1,9 +1,14 @@
 package br.com.emmanuelneri.monolitica.Veiculo;
 
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
@@ -12,6 +17,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import br.com.emmanuelneri.monolitica.model.Modelo;
 import br.com.emmanuelneri.monolitica.model.Veiculo;
 import br.com.emmanuelneri.monolitica.service.VeiculoService;
 
@@ -21,11 +27,42 @@ public class VeiculoServiceUnitTest {
 
 	@Mock Veiculo veiculo;
 	@Mock VeiculoService veiculoService;
+	@Mock Modelo modelo;
+	
+	ArrayList<Veiculo> veiculos;
+	HashMap<Modelo, Collection<Veiculo>> teste;
+	
+	@Before
+	public void setUp() {
+		veiculos = new ArrayList<Veiculo>();
+		
+		veiculo.setNome("aaaa");
+		veiculo.setModelo(modelo);
+		veiculos.add(veiculo);
+		
+		veiculo.setNome("bbbb");
+		veiculo.setModelo(modelo);
+		veiculos.add(veiculo);
+		
+		veiculo.setNome("ccc");
+		veiculo.setModelo(modelo);
+		veiculos.add(veiculo);
+		
+		teste = new HashMap<Modelo,Collection<Veiculo>>();
+		teste.put(modelo, veiculos);
+	}
 	
 	@Test
 	public void TestaCadastrarVeiculo() {
+		Mockito.lenient().doNothing().when(veiculoService).save(veiculo);
 		veiculoService.save(veiculo);
-		Mockito.lenient().when(veiculoService.findById(veiculo.getId())).thenReturn(veiculo);
-		verify(veiculoService, atMost(10)).save(veiculo);
+		verify(veiculoService,atMost(2)).save(veiculo);
 	}
+	
+	@Test
+	public void TestafindVeiculosPorModeloNaoNulo() {
+		Mockito.lenient().when(veiculoService.findVeiculosPorModelo()).thenReturn(teste);
+		assertNotNull(veiculoService.findVeiculosPorModelo());
+	}
+	
 }
